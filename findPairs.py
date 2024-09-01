21e10ed8e7decb1fcff8fcc37a545645969a5ec4
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-09-01 12:38:02 +0530

    Added find pairs problem

---
+++ findPairs.py
@@ -0,0 +1,11 @@
+# Write a program to find all possible pairs of numbers whose product is 1947. Store them as list of list. (eg: [[1,1947],[33,59]]) 
+def find_pairs(product):
+    pairs = []
+    for i in range (1,int(product ** 0.5)+1):
+        if product % i ==0:
+            pairs.append([i , product // i])
+    return pairs
+
+product = 1947
+pairs= find_pairs(product)
+print(pairs)
